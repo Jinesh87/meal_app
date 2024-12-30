af18030eece2a8b4b867d8b73d49b486a4b64031
Jinesh87 <zodiacaus@gmail.com>
2024-12-30 12:32:09 +0530

    Adding Icon Button On Header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,31 @@
-import { Image, Text, View, StyleSheet ,ScrollView} from "react-native";
+import { useLayoutEffect } from "react";
+import { Image, Text, View, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from '../data/dummy-data';
 import MealDetails from "../components/MealDetails";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
+import IconButton from "../components/IconButton";
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-    return <ScrollView style={styles.rootContainer}>  
+    function headerButtonPressHandler() {
+        console.log('Pressed!');
+    }
+
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />
+                );
+            },
+        });
+    }, [navigation, headerButtonPressHandler]);
+
+    return <ScrollView style={styles.rootContainer}>
         <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
         <Text style={styles.title}>{selectedMeal.title}</Text>
         <MealDetails
@@ -33,7 +50,7 @@ export default MealDetailScreen;
 const styles = StyleSheet.create({
     rootContainer: {
         marginBottom: 32
-      },
+    },
     image: {
         width: '100%',
         height: 350
